bac9eb7034cb83423533162b8d2b8c7d0a7e603a
Aadhi22 <77587184+Aadhi22@users.noreply.github.com>
2024-10-02 23:36:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -21,3 +21,101 @@ function Filteryear() {
     }
   }
 }
+
+function Filtermovie() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInputmovie");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++)
+  {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td)
+    {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1)
+      {
+        tr[i].style.display = "";
+      }
+      else
+      {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+
+function Filterlang() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInputlang");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++)
+  {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td)
+    {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1)
+      {
+        tr[i].style.display = "";
+      }
+      else
+      {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function Filterrate() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInputrate");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++)
+  {
+    td = tr[i].getElementsByTagName("td")[3];
+    if (td)
+    {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1)
+      {
+        tr[i].style.display = "";
+      }
+      else
+      {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+
+function Filterprint() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInputprint");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++)
+  {
+    td = tr[i].getElementsByTagName("td")[4];
+    if (td)
+    {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1)
+      {
+        tr[i].style.display = "";
+      }
+      else
+      {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
